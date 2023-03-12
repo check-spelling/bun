49168b30f435e44f5d104cbed376679a31bfa786
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-12 19:25:18 -0400

    spelling: preemptively
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/bun-types/crypto.d.ts
+++ packages/bun-types/crypto.d.ts
@@ -3741,7 +3741,7 @@ declare module "crypto" {
   interface RandomUUIDOptions {
     /**
      * By default, to improve performance,
-     * Node.js will pre-emptively generate and persistently cache enough
+     * Node.js will preemptively generate and persistently cache enough
      * random data to generate up to 128 random UUIDs. To generate a UUID
      * without using the cache, set `disableEntropyCache` to `true`.
      *
