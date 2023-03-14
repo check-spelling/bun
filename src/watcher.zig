cdf9ec4d735b83d02d0ad335577a799046608662
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-13 20:28:26 -0400

    spelling: coalesce
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/watcher.zig
+++ src/watcher.zig
@@ -488,7 +488,7 @@ pub fn NewWatcher(comptime ContextType: type) type {
                         null,
                     );
 
-                    // Give the events more time to coallesce
+                    // Give the events more time to coalesce
                     if (count_ < 128 / 2) {
                         const remain = 128 - count_;
                         var timespec = std.os.timespec{ .tv_sec = 0, .tv_nsec = 100_000 };
